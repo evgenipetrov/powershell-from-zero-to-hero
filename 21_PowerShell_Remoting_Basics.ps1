e6e0f3787834c21364fae9a5d8d9b44d071769e1
evgeni <evgeni.petrov@gmail.com>
2017-02-23 00:39:12 -0800

    Started module.

---
+++ 21_PowerShell_Remoting_Basics.ps1
@@ -0,0 +1,16 @@
+﻿<# 00. POWERSHELL REMOTING BASICS #>
+
+## 1. How it works
+
+# Protocol
+# WS-MAN - web services for management. WinRM. HTTP(S). 5985 / 5986
+
+# Interactive usage - 1:1
+# One to many - 1:n
+
+## 2. Enabling manually
+
+## 3. Enabling via GPO
+
+## 4. Basic usage
+
