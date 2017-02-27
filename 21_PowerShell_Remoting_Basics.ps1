f3638295a7309c318806082bed9f2c147bce5d66
evgeni <evgeni.petrov@gmail.com>
2017-02-27 16:22:47 +0200

    wip as I am switching computers.

--- 21_PowerShell_Remoting_Basics.ps1
+++ 21_PowerShell_Remoting_Basics.ps1
@@ -10,7 +10,23 @@
 
 ## 2. Enabling manually
 
+# enable it with PowerShell command
+Enable-PSRemoting
+
+# enable it with CMD 
+winrm quickconfig
+
 ## 3. Enabling via GPO
+
+# location: Computer Configuration > Policies > Administrative Templates > Windows Components > Windows Remote Management > WinRM Service
+# filters
+
+# set WinRM service to automatic. This will not start the service. How can we start it immediately?
+# location: Computer Configuration > Policies > Windows Settings > Security Settings > System Services > Windows Remote Management
+
+# configure a firewall exception.
+# location: Computer Configuration > Windows Settings > Administrative Templates > Network > Network Connections > Windows Firewall > Domain Profile > Windows Firewall: Allow local port exceptions. / Windows Firewall: Define inbound Port Exceptions > Port 5985
+
 
 ## 4. Basic usage
 
