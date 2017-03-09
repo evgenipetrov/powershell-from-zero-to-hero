228a082cde60092714e010f63a3f18cf23657e09
evgeni <evgeni.petrov@gmail.com>
2017-03-09 05:09:38 -0800

    finished lesson 30

--- 30_WMI_and_CIM_Using_WMI_Commands_to_Query_Data.ps1
+++ 30_WMI_and_CIM_Using_WMI_Commands_to_Query_Data.ps1
@@ -1,34 +1,52 @@
 ﻿<# 30. USING WMI COMMANDS TO QUERY DATA #>
 
-## 1. The Repository
+## 1. Getting WMI objects
 
-# demo - mmc -> WMI Control snap-in
+# get objects
+Get-WmiObject -Class win32_service
 
-# WMI Explorer: https://powershell.org/2013/03/08/wmi-explorer/
+## 2. They are objects!
 
-# Sapien WMI explorer : https://www.sapien.com/software/wmiexplorer
+# see all the properties. 
+Get-WmiObject -Class win32_service | Select-Object -First 1 -Property *
 
-## 2. Exploring the Repository
+# compare to get-service cmdlet output
+Get-Service | Select-Object -First 1 -Property *
 
-## 3. Finding Documentation
+# short version
+gwmi win32_bios
 
-# https://msdn.microsoft.com/en-us/library/aa394418(v=vs.85).aspx
+## 3. Other computers and credentials
 
-## 4. WMI vs. CIM
+# target multiple computers. note the slowness. note the query is sequential
+gwmi -ComputerName dc,member,localhost -Class win32_bios
 
-## 5. Finding classes in PowerShell
+# target multiple targets. note the actual machine name is not listed in the error.
+gwmi -ComputerName dc,nonexisting,localhost -Class win32_bios
 
-# show me everything
-Get-WmiObject -Namespace root\cimv2 -List
+# why this is even slower?
+gwmi -ComputerName dc,member,localhost -Class win32_bios | Format-Table -AutoSize
 
-# get one class instances
-Get-WmiObject -Namespace root\cimv2 -Class Win32_Service | Format-Table -AutoSize
+# PSComputername property. Only PS version 3.0
+gwmi -ComputerName dc,nonexisting,localhost -Class win32_bios | gm
 
-# try to find instances for disk management
-Get-WmiObject -Namespace root\cimv2 -List | Where-Object { $_.name -like "*disk*" }
+# querying other namespaces
+gwmi -Class AntiSpywareProduct
+gwmi -Class AntiSpywareProduct -Namespace root\securitycenter2
 
+# passing credentials. note the error with local connection
+gwmi -Class win32_operatingsystem -ComputerName client,dc -Credential lab\administrator
 
+# help on the get-wmiobjest
+help Get-WmiObject -Full
 
+# sometimes the documentation is useful to decode the WMI object values
+gwmi -Class win32_logicaldisk
 
+## 4. Working with property values
 
+# PowerShell adds its own methods to the major core WMI classes
+$os = Get-WmiObject -Class win32_operatingsystem
+$os.LastBootUpTime
+$os.ConvertToDateTime($os.LastBootUpTime)
 
