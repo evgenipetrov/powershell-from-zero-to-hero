f9df120c6a804ab9e21a53ed2c7005ed10829632
evgeni <evgeni.petrov@gmail.com>
2017-03-09 22:50:48 -0800

    Started lesson 31

---
+++ 31_WMI_and_CIM_Using_CIM_Commands_to_Query_Data.ps1
@@ -0,0 +1,19 @@
+﻿<# 31. USING CIM COMMANDS TO QUERY DATA #>
+
+## 1. Getting CIM objects
+
+# CIM is only available in PS v3.0+ 
+Get-Command -Noun cim*
+
+# Class definition vs Objest
+help Get-CimClass
+
+## 2. Getting instances
+
+## 3. Working with command options
+
+## 4. working with output
+
+## 5. Based on WS-MAN
+
+
