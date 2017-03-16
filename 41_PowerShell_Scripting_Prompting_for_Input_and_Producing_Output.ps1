3285b8b1b4d02f4489194cc49e407d9bc115467d
evgeni <evgeni.petrov@gmail.com>
2017-03-16 17:14:41 +0200

    still work in progress

--- 41_PowerShell_Scripting_Prompting_for_Input_and_Producing_Output.ps1
+++ 41_PowerShell_Scripting_Prompting_for_Input_and_Producing_Output.ps1
@@ -1,87 +1,9 @@
 ﻿<# 41. PROMPTING FOR INPUT AND PRODUCING OUTPUT #>
 
-## 0. Preparation
+## 1. Writig to the screen is not output
 
-Set-ExecutionPolicy -ExecutionPolicy Restricted
+## 2. Creating a file on disk is not output
 
-## 1. Running scripts
+## 3. Write Host, Output, Verbose, etc
 
-# see the sample.ps1 script. try to open it.
-
-# try to copy paste the content in the PowerShell console
-
-## 2. Filename extension association
-
-# try to right click it and run it
-
-# try to run it from the console. note that to run scripts in the console you always have to use some form of path.
-
-# what is a certificate authority? what is class 3 certificate?
-
-# how do you sign a script?
-Get-Help Set-AuthenticodeSignature -Full
-
-# see an example of a signature
-Get-Content C:\Windows\System32\WindowsPowerShell\v1.0\DotNetTypes.format.ps1xml
-
-# see the signature itself
-Get-AuthenticodeSignature C:\Windows\System32\WindowsPowerShell\v1.0\DotNetTypes.format.ps1xml | Format-List -Property *
-
-## 3. Execution policy
-
-# See the execution policy
-help Set-ExecutionPolicy
-
-# what are the defaults?
-# before server 2012 - Restricted
-# after server 2012 - RemoteSigned
-
-# any user can run CMD with any policy
-cmd.exe /c "powershell.exe -ExecutionPolicy Bypass"
-Get-ExecutionPolicy
-exit
-Get-ExecutionPolicy
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## Answers
-
-# 1.
-Get-WmiObject -Class win32_logicaldisk -Filter "drivetype=3"
-# 2.
-Get-CimInstance -ClassName win32_process -ComputerName client,dc
-# 3.
-Get-WmiObject -Class win32_nteventlogfile -Filter "logfilename='system'" -EnableAllPrivileges -ComputerName win7.lab.pri | Invoke-WmiMethod -Name BackupEventLog -ArgumentList c:\backup.evt
-# 4. 
-$win7 = New-CimSession -ComputerName win7.lab.pri -SessionOption (New-CimSessionOption -Protocol Dcom)
-Get-CimInstance -CimSession $win7 -ClassName win32_bios
-
+## 4. Read Host
