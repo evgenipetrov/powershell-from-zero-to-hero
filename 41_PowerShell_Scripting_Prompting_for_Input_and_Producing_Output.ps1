948c904f393e2b72ce9ff0a59939f3bbb0d7eca4
evgeni <evgeni.petrov@gmail.com>
2017-03-17 10:39:21 +0200

    WIP

--- 41_PowerShell_Scripting_Prompting_for_Input_and_Producing_Output.ps1
+++ 41_PowerShell_Scripting_Prompting_for_Input_and_Producing_Output.ps1
@@ -1,9 +1,7 @@
 ﻿<# 41. PROMPTING FOR INPUT AND PRODUCING OUTPUT #>
 
-## 1. Writig to the screen is not output
+## 1. What is Output in PowerShell terms?
 
-## 2. Creating a file on disk is not output
+## 2. Write Host, Output, Verbose, etc
 
-## 3. Write Host, Output, Verbose, etc
-
-## 4. Read Host
+## 3. Read Host
