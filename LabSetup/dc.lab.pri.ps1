dbcb27079a740acbfee5f47f6691c9efd40a6e0c
evgeni <evgeni.petrov@gmail.com>
2017-03-27 11:40:50 -0700

    Added DC bits installation to dc.lab.pri setup script

---
+++ LabSetup/dc.lab.pri.ps1
@@ -0,0 +1,17 @@
+﻿$computename = 'DC'
+
+$IPv4Address = '192.168.10.2'
+$prefixLength = '24'
+$dns = '192.168.10.2'
+
+
+
+Import-Module -Name $PSScriptRoot\..\LabTools\LabTools.psd1 -Force -Verbose
+
+Rename-LabComputer -NewName $computename
+Set-LabNetAddress -IPv4Address $IPv4Address -PrefixLength $prefixLength -DNS $dns
+Install-LabWindowsServerFeature -Name 'AD-Domain-Services'
+
+
+
+
