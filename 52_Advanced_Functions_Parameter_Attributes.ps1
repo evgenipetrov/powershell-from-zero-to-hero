b0cddf44dd29a0cb1175f3598fc1dfa19c7c497e
evgeni <evgeni.petrov@gmail.com>
2017-03-28 15:18:24 -0700

    Finished lesson 52

---
+++ 52_Advanced_Functions_Parameter_Attributes.ps1
@@ -0,0 +1,52 @@
+﻿#requires -Version 3.0
+<# 52. ADVANCED FUNCTIONS: PARAMETER ATTRIBUTES #>
+
+## 0. Preparation: Adding custom module path
+Remove-Module MyTools
+
+$customModulePath = "{0}\Scripts\Lesson_52\" -f $PSScriptRoot
+if ($env:PSModulePath -notlike "*$customModulePath*" )
+{
+  $env:PSModulePath = $env:PSModulePath + ";$customModulePath"
+}
+
+$env:PSModulePath
+
+## 1. Mandatory parameters
+
+param
+(
+  [Parameter(Mandatory=$true)]
+  [string[]]$Parameter
+)
+
+## 2. Help messages
+param
+(
+  [Parameter(HelpMessage='Computer name to query')]
+  [string[]]$Parameter
+)
+
+## 3. Positional parameters
+param
+(
+  [Parameter(Position=1)]
+  [string[]]$Parameter
+)
+
+## 4. Parameter Validation
+param
+(
+  [ValdateSet('OptionA','OptionB')]
+  [ValidatePattern('\w+\.lab\.pri')]
+  [ValidateLength(1,100)]
+  [ValidateCount(1,5)]
+  [string[]]$Parameter
+)
+
+## 5. Parameter aliases
+param
+(
+  [Alias('hostname')]
+  [string[]]$Parameter
+)
---
+++ Scripts/Lesson_52/MyTools/MyTools.psm1
@@ -0,0 +1,86 @@
+﻿function Get-DiskSpaceInfo
+{
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory = $true,
+                HelpMessage = 'Computer name to query')]
+    [Alias('Hostname')]
+    [ValidatePattern('\w+\.lab\.pri')]
+    [String[]]$ComputerName,
+    
+    [Parameter()]
+    [Switch]$LocalOnly = $true,
+    
+    [Parameter(Position = 2)]
+    [ValidateSet('Floppy','Local','Optical')]
+    [string]$DriveType = 'Local'
+  )
+  
+  begin{}
+  
+  process{
+    foreach ($computer in $ComputerName)
+    {
+      $params = @{
+        'ComputerName' = $computer
+        'Class'      = 'Win32_LogicalDisk'
+      }
+      
+      switch ($DriveType)
+      {
+        'Local'      
+        {
+          $params.Add('Filter', 'DriveType=3')
+          break
+        } 
+        'Floppy'     
+        {
+          $params.Add('Filter', 'DriveType=2')
+          break
+        }
+        'Optical'    
+        {
+          $params.Add('Filter', 'DriveType=5')
+          break
+        }
+      }
+      
+      
+      if ($LocalOnly)
+      {
+        $params.Add('Filter','DriveType=3')
+      }
+      
+      Get-WmiObject @params |
+      Select-Object @{
+        n = 'Drive'
+        e = {
+          $_.DeviceID
+        }
+      }, 
+      @{
+        n = 'Size'
+        e = {
+          "{0:N2}" -f ($_.Size)
+        }
+      }, 
+      @{
+        n = 'FreeSpace'
+        e = {
+          "{0:N2}" -f ($_.FreeSpace)
+        }
+      }, 
+      @{
+        n = 'FreePercent'
+        e = {
+          "{0:N2}" -f ($_.FreeSpace / $_.Size * 100)
+        }
+      }
+    }
+  }
+  
+  end {}
+}
+
+
