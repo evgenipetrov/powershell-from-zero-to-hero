4965f6b8cd5c902ac9528b3ba3d0459dfac7f5e4
evgeni <evgeni.petrov@gmail.com>
2019-01-20 18:20:09 +0200

    lesson 1

---
+++ lessons/01 Introduction and Lab Setup/Lesson.ps1
@@ -0,0 +1,23 @@
+## 1. Course Outline
+
+# PowerShell Core
+# PowerShell Remoting
+# WMI and CIM
+# Jobs
+# Scripting in PowerShell
+# Advanced Functions
+# Advanced Scripting Techniques
+# PowerShell in the Field
+# PowerShell Workflows
+# Desired State Configuration
+# Writing Scripts for Other People
+# Advanced Core Techniques, Tips, Tricks
+
+## 2. Setup
+
+# Smartgit, or any other git tool
+
+## 3. Virtual Machines
+
+# see lab inventory in setup folder
+# v2, v3, v4
