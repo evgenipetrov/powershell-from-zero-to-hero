2deb331f824b210778f0b9bc46c27e7f0f7e8084
evgeni <evgeni.petrov@dxc.com>
2019-02-05 21:14:53 +0200

    fixed typos in lesson 15

--- lessons/15 Comparison Operators and Filtering/Lesson.ps1
+++ lessons/15 Comparison Operators and Filtering/Lesson.ps1
@@ -5,7 +5,7 @@
 
 5 -ne 6
 
-# comparison operators, msdn
+# Comparison operators
 # -gt -lt -ge -le -like
 
 "hello" -eq "hello"
@@ -16,9 +16,7 @@
 "hello" -like "*l*"
 "hello" -contains "l" # for arrays only!
 
-# test case sensitivity
-
-# get services and check if a certain service is running
+# Case sensitivity
 
 ## Filtering items out of pipeline
 
@@ -27,24 +25,23 @@ Get-Service | Where-Object -FilterScript {$_.Name -eq "BITS"} | Select-Object -E
 Get-Service | Where-Object -FilterScript {$_.Name -eq "BITS"} | Select-Object -Property status, Name | fl
 Get-Service | Select-Object -Property status, Name | Where-Object -FilterScript {$_.Name -eq "BITS"} 
 
-# aliases
-# ? , where
+# aliases: ? , where
 # $_ vs $PSItem
 Get-Service | Select-Object -Property status, Name | Where-Object -FilterScript {$_.Name -eq "BITS"} 
 Get-Service | Select-Object -Property status, Name | Where-Object -FilterScript {$PSItem.Name -eq "BITS"} 
 
-# can we filter multiple times? objects | where | where | where
+# Multi filtering | where | where | where
 Get-Service | ? {$_.Name -like "a*"} | where {$PSItem.Status -ne "running"} # less efficient
 Get-Service | ? {$_.Name -like "a*" -and $_.Status -eq "stopped"} # more efficient and faster
 
 Get-Service | Where-Object -Property X -EQ -Value Y
-# filter left/format right
-# internal vs external filters
+# Rule: Filter to the left, format to the right
+# Internal vs. external filters
 Get-Process -Name svchost # more efficient and quicker
 Get-Process | where 'name' -eq 'svchost' # less efficient and quicker
 
 ## Basic boolean operators
 # -and -or
-# $true/$false do we need to compare them?
+# $true and $false; do we need to compare them?
 
-# -not operator
+# Inversions: -not operator
