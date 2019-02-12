637720f2525fb55150ad1e9fb7a96b02706345d0
evgeni <evgeni.petrov@dxc.com>
2019-02-12 14:36:19 +0200

    lessons 19 and 20 finished.

---
+++ lessons/19 Completing a Real World Task/Lesson.ps1
@@ -0,0 +1,7 @@
+## How to approach?
+# Discover, Learn, Experiment, Revise, Complete
+
+# Task: Get a list of installed roles and features on a server machine, a baseline. 
+# Save the baseline to a file. 
+# Then be able to uninstall any features that are not compliant with the baseline.
+# Make it automatic.
